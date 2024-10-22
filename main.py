2f27936c6328dfb625477a1903d1b631852da5a0
zipsonic <rick@hail3.com>
2024-10-22 04:38:23 -0700

    updated to groups for update and draw - also arrow keys

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ def main():
     pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    updatable.add(player)
+    drawable.add(player)
+    
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
-        player.update(dt)
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = pygame.time.Clock().tick(60) / 1000
 
--- player.py
+++ player.py
@@ -36,6 +36,18 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-abs(dt))
 
+        if keys[pygame.K_LEFT]:
+            self.rotate(dt)
+            # ?
+        if keys[pygame.K_RIGHT]:
+            self.rotate(-abs(dt))
+            # ?
+        if keys[pygame.K_UP]:
+            self.move(dt)
+        
+        if keys[pygame.K_DOWN]:
+            self.move(-abs(dt))
+
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
