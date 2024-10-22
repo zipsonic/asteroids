a46ca45ce9a941e2fcb3471ddf08cd8061e038f8
zipsonic <rick@hail3.com>
2024-10-22 08:08:32 -0700

    asteroids can hit me now

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision_check(self,other):
+        return (other.radius + self.radius) > self.position.distance_to(other.position)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -10,21 +12,34 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroids = pygame.sprite.Group()
 
-    updatable.add(player)
-    drawable.add(player)
-    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    field = AsteroidField()
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
+
+        #updatable.update(dt)
+        #drawable.draw(screen)
+        
         for obj in updatable:
             obj.update(dt)
         for obj in drawable:
             obj.draw(screen)
+        for obj in asteroids:
+            if obj.collision_check(player):
+                print("Game Over!")
+                running = False
+
         pygame.display.flip()
         dt = pygame.time.Clock().tick(60) / 1000
 
